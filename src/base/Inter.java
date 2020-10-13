3a8436652fb6c04bfd85effe08fd54204ff26dcb
Dima <doloshkan96@mail.ru>
2020-10-13 17:47:44 +0300

    commit ex

---
+++ src/base/Inter.java
@@ -0,0 +1,12 @@
+package base;
+
+public interface Inter {
+
+//    Ex 18.1
+//    String apply(int a);
+//
+//    Ex 18.2
+//    String apply(String str);
+
+
+}
---
+++ src/base/Main.java
@@ -0,0 +1,57 @@
+package base;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+//        Ex 18.1
+//        Inter inter1 = a -> {
+//            if (a == 0)
+//                return "equal to zero";
+//            else if (a > 0)
+//                return "greater than zero";
+//            return "less than zero";
+//        };
+//
+//        String str1 = inter1.apply(-1);
+//        System.out.println(str1);
+//
+//        Inter inter2 = a -> {
+//            if (a % 2 == 0)
+//                return "even number";
+//            return "odd number";
+//        };
+//
+//        String str2 = inter2.apply(2);
+//        System.out.println(str2);
+
+
+//        Ex 18.2
+//        Inter inter1 = str -> {
+//            if (str.contains("knowledge"))
+//                return "contains";
+//            return "doesn't contain";
+//        };
+//
+//        Inter inter2 = str -> {
+//            if (str.length() > 10)
+//                return " length greater than 10";
+//            return " length less than 10";
+//        };
+//
+//        Inter inter3 = str -> {
+//            if (str.trim().isEmpty())
+//                return "is empty";
+//            return "isn't empty";
+//        };
+//
+//        String str = inter3.apply("        ");
+//        System.out.println(str);
+
+
+
+    }
+
+
+}
